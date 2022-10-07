5946100d0b5dac123324145ab642bf3661dd5257
Vamsi kundeti <kundetivamsi2001@gmail.com>
2022-10-07 11:24:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 import warnings
 
 
-st.beta_set_page_config(page_title="Crop Recommender", page_icon="🌿", layout='centered', initial_sidebar_state="collapsed")
+st.beta_set_page_config(page_title="Crop Recommender", page_icon="🌿", layout='right', initial_sidebar_state="collapsed")
 
 def load_model(modelfile):
 	loaded_model = pickle.load(open(modelfile, 'rb'))
